0c3290fccda934dd9bc2a427501a63f4ae3b7a49
Jan Hendrickx <jan.hendrickx@beresterk.be>
2018-09-26 15:15:43 +0200

    Craft update with Beresterk redactor JSON file

--- config/general.php
+++ config/general.php
@@ -8,38 +8,66 @@
  * @see craft\config\GeneralConfig
  */
 
+
+/** HTTP TO HTTPS redirect without any changes in the template or any other file - TO BE TESTED
+* Ensure our urls have the right scheme
+* define('URI_SCHEME',  ( isset($_SERVER['HTTPS'] ) ) ? "https://" : "http://" );
+* The site url
+* define('SITE_URL',    URI_SCHEME . $_SERVER['SERVER_NAME'] . '/');
+**/
+// the unix timestamp on which the style.css file has been uploaded the last time - used to bust caching for the last uploaded version of the css file
+define('CSSTIME', ('t='. filemtime('../www/css/style.css')));
+//define('FILEMTIME',  date(U)) ;
+// the unix timestamp on which the webflow.js file has been uploaded the last time - used to bust caching for the last uploaded version of the js file
+define('JSTIME', ('t='. filemtime('../www/js/webflow.js')));
+
+
 return [
     // Global settings
+
+
+
     '*' => [
+        'cssTimestamp' => CSSTIME,
+        'cssVersion' => '1.0',
+        'jsTimestamp' => JSTIME,
+        'jsVersion' => '1.0',
         // Default Week Start Day (0 = Sunday, 1 = Monday...)
         'defaultWeekStartDay' => 0,
-
         // Enable CSRF Protection (recommended)
         'enableCsrfProtection' => true,
-
         // Whether generated URLs should omit "index.php"
         'omitScriptNameInUrls' => true,
-
         // Control Panel trigger word
-        'cpTrigger' => 'admin',
-
+        'cpTrigger' => 'beresterk',
+        'extraAllowedFileExtensions' => 'json',
         // The secure key Craft will use for hashing and encrypting data
         'securityKey' => getenv('SECURITY_KEY'),
+        'phpMaxMemoryLimit' => '512M',
     ],
 
     // Dev environment settings
-    'dev' => [
-        // Base site URL
-        'siteUrl' => null,
-
+    '.webhosting.be' => [
         // Dev Mode (see https://craftcms.com/support/dev-mode)
         'devMode' => true,
+        // Base site URL
+        'siteUrl' => 'beresterkebasisbe.webhosting.be/',
+
+        'aliases' => [
+            '@basePath' => '/data/sites/web/beresterkebasisbe/www/assets',
+            '@baseUrl' => 'beresterkebasisbe.webhosting.be/',
+        ],
+
     ],
 
     // Staging environment settings
-    'staging' => [
+    '.be' => [
+        'siteUrl' => 'beresterkebasis.be/',
         // Base site URL
-        'siteUrl' => null,
+        'aliases' => [
+            '@basePath' => '/data/sites/web/beresterkebasisbe/www/assets',
+            '@baseUrl' => 'http://beresterkebasis.be/',
+        ],
     ],
 
     // Production environment settings
